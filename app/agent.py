8fb78f30fe420bb6820011c4889f0bccf6fb10ab
--show-origin <deepyes@outlook.com>
2025-06-04 13:55:50 +0900

    File cleanups

--- app/agent.py
+++
@@ -1,7 +0,0 @@
-from app.langgraph_setup import app as agent_app
-from langchain_core.messages import HumanMessage
-
-def run_agent(prompt: str):
-    input_msg = {"messages": [HumanMessage(content=prompt)]}
-    response = agent_app.invoke(input_msg)
-    return response["messages"][-1].content
--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ from app.call_the_model import call_the_model
 
 app = FastAPI()
 
+@app.get("/")
+def home():
+  return {"response": "Hello world"}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+  return {"response", user_id}
+
 @app.post("/chat")
 async def chat(request: Request):
     body = await request.json()
