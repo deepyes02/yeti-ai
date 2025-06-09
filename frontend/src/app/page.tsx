c26a1572246f03301c09ca70a14d7c4cb87f5d42
--show-origin <deepyes@outlook.com>
2025-06-09 16:37:25 +0900

    Trying to separate thinking from answer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,6 +6,7 @@ import styles from "./page.module.scss";
 type RoleAndMessage = {
   role: 'ai' | 'user';
   content: string;
+  think?: string;
 };
 
 export default function Home() {
@@ -13,25 +14,27 @@ export default function Home() {
   const [input, setInput] = useState("");
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const socket = useRef<WebSocket | null>(null);
+  const aiMessageBuffer = useRef("")
   useEffect(() => {
-    socket.current = new WebSocket("ws://localhost:8000/ws");
-    // socket.current = new WebSocket("ws://localhost:8000/ws-decoy");
+    // socket.current = new WebSocket("ws://localhost:8000/ws");
+    socket.current = new WebSocket("ws://localhost:8000/ws-decoy");
     socket.current.onmessage = (event) => {
-      const text = event.data;
 
+      const text = event.data;
+      console.log('Received chunk:', JSON.stringify(text));
+      // console.log(text)
       setMessages((prevMsgs) => {
         const newMsgs = [...prevMsgs];
         const lastIndex = newMsgs.length - 1;
-
-        // console.log(newMsgs)
+        aiMessageBuffer.current += text
 
         // If it's an AI continuation
         if (newMsgs[lastIndex]?.role === "ai") {
-          if (!newMsgs[lastIndex].content.endsWith(text)) {
-            newMsgs[lastIndex].content += text;
+            newMsgs[lastIndex] = {
+              ...newMsgs[lastIndex],
+              content: aiMessageBuffer.current
+            }
           }
-        }
-
         return newMsgs;
       });
     };
@@ -43,6 +46,7 @@ export default function Home() {
     if (e) e.preventDefault();
     if(!input.trim()) return;
     const userMsg = { role: "user" as const, content: input };
+    aiMessageBuffer.current = "";
     //setMessages and create a new placeholder for ai response
     setMessages((msgs) => [...msgs, userMsg, {role: "ai", content:""}]);
     socket.current?.send(input);
