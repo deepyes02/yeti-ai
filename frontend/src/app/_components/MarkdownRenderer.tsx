28cbc271aaaf237092626f8531552df8052ab6ca
--show-origin <deepyes@outlook.com>
2025-06-09 21:20:17 +0900

    Updated readme

--- frontend/src/app/_components/MarkdownRenderer.tsx
+++ frontend/src/app/_components/MarkdownRenderer.tsx
@@ -1,20 +1,14 @@
-// components/MarkdownRenderer.tsx
 'use client'
-
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeHighlight from 'rehype-highlight'
-import 'highlight.js/styles/stackoverflow-light.css';
+import 'highlight.js/styles/github-dark.css'
+import styles from "./MarkdownRenderer.module.scss"
+// import "../global.scss"
 
 export default function MarkdownRenderer({ content }: { content: string }) {
-  return (
-    <div className="prose max-w-none dark:prose-invert">
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[ rehypeHighlight]}
-      >
-        {content}
-      </ReactMarkdown>
-    </div>
+  return ( 
+    <div className={styles.markdownContainer}><ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>{content}</ReactMarkdown></div>
   );
 }
+
