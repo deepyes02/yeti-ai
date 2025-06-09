43de7ce728d9ff2e681dcd9363e79dd7a60fb4db
--show-origin <deepyes@outlook.com>
2025-06-09 18:57:29 +0900

    Markdown rendered with highlight js

--- app/call_the_model.py
+++ app/call_the_model.py
@@ -1,21 +1,15 @@
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessageChunk
 from langgraph.graph import START, MessagesState, StateGraph, END
 import logging
 import os
+from app.utils.system_prompt import system_prompt
 
 ## pip install -U "psycopg[binary,pool]" langgraph langgraph-checkpoint-postgres ##
 from langgraph.checkpoint.postgres import PostgresSaver
 conn = "postgresql://deepyes02:yEti-2025-yAk-ai@db:5432/ai_agent"
 
-prompt_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", f"You are yeti. Be concise and short when possible. Answer the question asked directly. Study user content and respond accordingly. Do not share or encourage harmful and political information that might create social instability. Encourage people to protect their privacy and stay safe. Always make sure no legal boundaries are crossed. Caringly warn users to be reflective of their actions when it suits so, not only to judge on the boundaries they cross, but to assure them that what they shared was private, and the warning was a sense of caution rather than dictating them. Do not provide false flattery, but authentic appreciation and encouragement. Healthy AI. If you haven't already, ask the user their name. Check input for repeat questions. If same question is repeated, ask for clarifications, so that you are almost never stuck on a loop."),
-        MessagesPlaceholder(variable_name="messages"),
-        MessagesPlaceholder(variable_name="messages")
-    ]
-  )
+prompt_template = system_prompt()
 
 models = [
   "qwen3",
@@ -26,7 +20,7 @@ models = [
 ]
 model = ChatOllama(
   base_url=os.getenv("OLLAMA_BASE_URL","http://host.docker.internal:11434"),
-  model=models[0],
+  model=models[1],
   num_ctx=12000,
   temperature=0.3,
   top_p=0.7,
@@ -38,7 +32,11 @@ def call_model(state: MessagesState):
   return {"messages": state["messages"] + [response]} 
 
 
-
+thread_id = 4
+workflow = StateGraph(state_schema=MessagesState)
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+workflow.add_edge("model", END)
 # We are now using sqlite to remember the context and hence for the agent to remember us by our 
 # user id 
 # from langgraph.checkpoint.sqlite import SqliteSaver
@@ -49,11 +47,7 @@ def stream_model_output_new(prompt:str):
   ##session handler
 
   with PostgresSaver.from_conn_string(conn) as checkpointer:
-    thread_id = 3
-    workflow = StateGraph(state_schema=MessagesState)
-    workflow.add_edge(START, "model")
-    workflow.add_node("model", call_model)
-    workflow.add_edge("model", END)
+    checkpointer.setup()
     app = workflow.compile(checkpointer=checkpointer)
     # This function is for streaming the output of the model
     state = {"messages" : [HumanMessage(content=prompt)]}
--- frontend/src/app/_components/MarkdownRenderer.tsx
+++ frontend/src/app/_components/MarkdownRenderer.tsx
@@ -1,25 +1,20 @@
 // components/MarkdownRenderer.tsx
 'use client'
 
-// import ReactMarkdown from 'react-markdown'
-// import remarkGfm from 'remark-gfm'
-// import rehypeRaw from "rehype-raw";
-// import rehypeHighlight from 'rehype-highlight'
-import 'highlight.js/styles/github.css' // choose any style from highlight.js
-import "../globals.scss"
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/stackoverflow-light.css';
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
-    // <div className="prose max-w-none dark:prose-invert">
-    //   <ReactMarkdown
-    //     remarkPlugins={[remarkGfm]}
-    //     rehypePlugins={[ rehypeHighlight]}
-    //   >
-    //     {content}
-    //   </ReactMarkdown>
-    // </div>
-    <div>
-      {content}
+    <div className="prose max-w-none dark:prose-invert">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[ rehypeHighlight]}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
