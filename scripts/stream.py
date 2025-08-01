7bc394e23e2a10448835f3871f70a857cd1a1d89
deepyes <deepyes02@outlook.com>
2025-08-01 15:40:26 +0900

    Config(ChatOpenAI) : Replaced stream.py with ChatOPENAI protocol and llama_cpp

--- scripts/stream.py
+++ scripts/stream.py
@@ -1,13 +1,17 @@
-from langchain_ollama import ChatOllama
+# from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
+from pydantic import SecretStr
+from langchain_core.runnables import RunnableConfig
 
-# db
-# from langgraph.checkpoint.sqlite import SqliteSaver
-
-model = ChatOllama(
-    model="gemma3:4b", temperature=0.9, top_p=0.95, num_ctx=2048, repeat_penalty=1.2
+model = ChatOpenAI(
+    base_url="http://localhost:8080/v1",
+    model="mistral-nemo",
+    api_key=SecretStr("your_api_key_here"),
+    temperature=0.9,
+    top_p=0.95,
 )
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
@@ -26,12 +30,12 @@ workflow.add_node("model", call_model)
 # Add memory
 memory = MemorySaver()
 app = workflow.compile(checkpointer=memory)
-config = {"configurable": {"thread_id": "thread_1"}}
+config: RunnableConfig = {"configurable": {"thread_id": "thread_1"}}
 
-query = "Tell me a long story"
+query = "Hi, my name is Deepesh, what is your name?"
 input_messages = [HumanMessage(content=query)]
 # output = app.invoke({"messages": input_messages}, config)
-# output["messages"][-1].pretty_print()
+# output["messages"][-1].pretty_prin
 
 for chunk, metadata in app.stream(
     {"messages": input_messages}, config=config, stream_mode="messages"
