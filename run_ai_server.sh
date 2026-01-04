e2332d3ebb56c8931c2f00d2d2ff3a7ba46ca0c4
deepyes <deepyes02@outlook.com>
2026-01-04 22:32:50 +0900

    Fix(inference): Updated inference code to make read faster

--- run_ai_server.sh
+++ run_ai_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf --jinja -c 4096
+llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf --jinja -c 4096 -ngl 99 -fa
 
 
