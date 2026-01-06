1ea35aac598e5678d910da9e9e68a3285428f154
deepyes <deepyes02@outlook.com>
2026-01-06 14:47:00 +0900

    Fix(server): Adding parameters to see if optimizing is helpful

--- run_ai_server.sh
+++ run_ai_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf --jinja -c 32768 -ngl 99 -fa -cb --cache-type-k q4_0 --cache-type-v q4_0
+mkdir -p slots
+llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf --jinja -c 32768 -ngl 99 -fa -cb --cache-type-k q4_0 --cache-type-v q4_0 -t 6 -tb 6 -b 2048 -ub 2048 --mlock --slot-save-path ./slots
 
 
