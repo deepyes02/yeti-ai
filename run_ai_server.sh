7c70a1e769e746b12563cda34cdca3c65c135b19
deepyes <deepyes02@outlook.com>
2026-01-06 14:57:02 +0900

    Fix(chore): Rewrote llama server runner

--- run_ai_server.sh
+++ run_ai_server.sh
@@ -2,4 +2,18 @@
 mkdir -p slots
 llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf --jinja -c 32768 -ngl 99 -fa -cb --cache-type-k q4_0 --cache-type-v q4_0 -t 6 -tb 6 -b 2048 -ub 2048 --mlock --slot-save-path ./slots
 
+# llama-server -m /Users/deepesh/Desktop/bin/llms/mistral-nemo-15.gguf \
+#   --jinja \
+#   -c 16000 \
+#   -ngl 99 \
+#   -fa \
+#   -cb \
+#   --cache-type-k q4_0 \
+#   --cache-type-v q4_0 \
+#   -t 8 \
+#   -tb 8 \
+#   -b 512 \
+#   -ub 512 \
+#   --mlock \
+#   --slot-save-path ./slots
 
